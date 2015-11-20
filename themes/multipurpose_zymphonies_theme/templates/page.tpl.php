a26f3ca11206ca2f54e90328a8e66fece844cbb5
EMerino236 <proteus236@gmail.com>
2015-11-20 15:21:58 -0500

    alejo se la come

--- themes/multipurpose_zymphonies_theme/templates/page.tpl.php
+++ themes/multipurpose_zymphonies_theme/templates/page.tpl.php
@@ -141,7 +141,7 @@
             <?php print $messages; ?>
             <?php print render($title_prefix); ?>
             <?php if ($title): ?>
-              <h1 class="page-title"><?php print $title; ?></h1>
+              <h1 class="page-title"><?php //print $title; ?></h1>
             <?php endif; ?>
             <?php print render($title_suffix); ?>
             <?php if (!empty($tabs['#primary'])): ?>
